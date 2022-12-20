523c878b03446b8692d6dd5c3b385e063fdd7871
lapkin.gleb <Laplinglen@gmail.com>
2022-12-21 03:43:59 +0400

    начало работы с тг ботом

---
+++ Servises/Program.cs
@@ -0,0 +1,40 @@
+﻿using Common.Configuration;
+using Common.Configuration.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Servises.BackGround;
+
+namespace Servises;
+
+internal class Program
+{
+	public static void Main(string[] args)
+	{
+		AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+		var host = CreateHostBuilder(args).Build();
+		var configuration = host.Services.GetService<IConfiguration>();
+		var loggerFactory = host.Services.GetService<ILoggerFactory>();
+
+		SharedConfiguration.UpdateSharedConfiguration(
+			configuration.GetConnectionString("DefaultConnectionString"),
+			configuration.GetSection("SmtpSettings").Get<SmtpConfiguration>(),
+			loggerFactory);
+
+		host.Run();
+	}
+
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+			.ConfigureServices((hostContext, services) =>
+			{
+				services.AddLogging(builder =>
+				{
+					builder.AddNLog();
+				});
+				services.AddHostedService<TelegramBotServise>();
+			}
+			);
+}
